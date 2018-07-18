f2aecf3547dfb5b1fb69d0c3e72b3c226e8d476d
jorgebaralt <jorgebaraltq@gmail.com>
2018-07-17 20:24:42 -0400

    set service account

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('create_user');
-
+const serviceAccount = require('service_account.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://servify-716c6.firebaseio.com"
