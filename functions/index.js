089bccefba01e5ddaa83b245b00eebf97667da3c
jorgebaralt <jorgebaraltq@gmail.com>
2018-07-17 20:13:57 -0400

    first commit

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+//admin is to access data in firebase
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const createUser = require('create_user');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://servify-716c6.firebaseio.com"
+});
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.createUser = functions.https.onRequest(createUser);
