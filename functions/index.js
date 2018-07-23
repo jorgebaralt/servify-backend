2c8f95baa03b5c5cb46e9225e58de43b6ff2ff0d
jorgebaralt <jorgebaraltq@gmaik.com>
2018-07-23 13:00:07 -0400

    staging everything

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 //admin is to access data in firebase
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const createUser = require('./create_user');
+const createUser = require('./create_user').default;
 const serviceAccount = require('./service_account.json');
 
 admin.initializeApp({
