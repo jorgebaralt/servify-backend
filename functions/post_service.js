1f00b38cc57a98b42649cc7a96e57c8ac15e948e
jorgebaralt <jorgebaraltq@gmaik.com>
2018-08-05 16:39:20 -0400

    Simplified post service, since the object received is object pushed to db, therefore, push at once.

--- functions/post_service.js
+++ functions/post_service.js
@@ -2,16 +2,8 @@ const admin = require('firebase-admin');
 
 module.exports = function(req,res){
     const db = admin.firestore();
-    const serviceTitle = req.body.serviceTitle;
-
-    db.collection('services').doc(serviceTitle).set({
-        category: req.body.category,
-        subcategory: req.body.category,
-        serviceTitle: serviceTitle,
-        phone: req.body.phone,
-        location: req.body.location,
-        description: req.body.description
-    }).then(result =>{
+    const newPost = req.body;
+    db.collection('services').add(newPost).then(result =>{
         return res.send(result);
     }).catch(error => {
         res.status(422).send({ error: error });
