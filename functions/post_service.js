9c5b43b56f307b3acb77954ae2952e8194ec0cf9
jorgebaralt <jorgebaraltq@gmaik.com>
2018-08-05 12:55:47 -0400

    post_service working

--- functions/post_service.js
+++ functions/post_service.js
@@ -1,21 +1,19 @@
 const admin = require('firebase-admin');
 
 module.exports = function(req,res){
-    const db = admin.firestore;
+    const db = admin.firestore();
     const serviceTitle = req.body.serviceTitle;
 
     db.collection('services').doc(serviceTitle).set({
-        category: req.body.selectedCategory.dbReference,
-        subcategory: req.body.selectedSubcategory.dbReference,
+        category: req.body.category,
+        subcategory: req.body.category,
         serviceTitle: serviceTitle,
         phone: req.body.phone,
         location: req.body.location,
         description: req.body.description
     }).then(result =>{
-        console.log('server success');
         return res.send(result);
     }).catch(error => {
-        console.log('server error');
         res.status(422).send({ error: error });
     });
 };
