1e2c7551a6c3e6b86f677b538b51b6d1ae9033cc
jorgebaralt <jorgebaraltq@gmaik.com>
2018-08-15 00:58:19 -0400

    Code clean up

--- functions/get_services.js
+++ functions/get_services.js
@@ -5,28 +5,32 @@ module.exports = function(req,res){
     const params = req.url.split('/');
     const category = params[1];
     const subcategory = params[2];
-    
-    db.collection('services').get()
-        .then(snapshot => {
-            res.send(snapshot);
-        }).catch(err => {
-            res.send(err);
-        });
+    let field,value = '';
 
-    // get all services for specific category
+    // decide category or subcategory
     if(subcategory){
-        db.collection('services').where('subcategory','==',subcategory).get()
-            .then(result => {
-                return res.send(result);
-            }).catch(err => {
-                return res.send(err);
-            });
-    } else if(category){
-        db.collection('services').where('category','==',category).get()
-            .then(result => {
-                return res.send(result);
-            }).catch(err => {
-                return res.send(err);
-            });
+        field = 'subcategory'
+        value = subcategory
+    } else if (category){
+        field = 'category'
+        value = category
+    } else {
+        return res.status(422).send({ error: 'nothing to query' });
     }
+
+    /*
+        get services for specific category/subcategory
+        take snapshot data, and put it into array
+        then return it
+    */
+    db.collection('services').where(field,'==',value).get()
+        .then(snapshot => {
+            var query = [];
+            snapshot.forEach( doc => {
+                query.push(doc.data());
+            })
+            return res.send(query);
+        }).catch(error => {
+            res.status(422).send({ error: error });
+        });
 };
