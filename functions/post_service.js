897f459581e5fe4937c42f24e204172f2e34bfd2
jorgebaralt <jorgebaraltq@gmaik.com>
2018-08-30 00:23:33 -0400

    added time stamp to db

--- functions/post_service.js
+++ functions/post_service.js
@@ -2,7 +2,9 @@ const admin = require('firebase-admin');
 
 module.exports = function(req,res){
     const db = admin.firestore();
+    const FieldValue = admin.firestore.FieldValue;
     const newPost = req.body;
+    newPost.timestamp = FieldValue.serverTimestamp();
     db.collection('services').add(newPost).then(result =>{
         return res.send(result);
     }).catch(error => {
