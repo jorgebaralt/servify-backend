1bef85c6424390aaf2d5c061d5b9cd8c8cc18c17
jorgebaralt <jorgebaraltq@gmaik.com>
2018-09-02 00:39:01 -0400

    updated add user to db

--- functions/add_user_db.js
+++ functions/add_user_db.js
@@ -2,16 +2,10 @@ const admin = require('firebase-admin');
 
 module.exports = function(req,res){
     const db = admin.firestore();
-    let displayName;
-    if(req.body.displayName.length > 0){
-        displayName = req.body.displayName;
-    } else {
-        displayName = req.body.firstName + " " + req.body.lastName;
-    }
     
     db.collection('users').doc(req.body.email).set({
         email: req.body.email,
-        fullName: displayName
+        fullName: req.body.displayName
     }).then(result => {
         return res.send(result);
     }).catch((error)=>{
