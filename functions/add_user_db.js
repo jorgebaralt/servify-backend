ec24ce18d3c03a8b47137c8829327424bc2b7172
jorgebaralt <jorgebaraltq@gmaik.com>
2018-10-15 10:51:09 -0400

    adding userId

--- functions/add_user_db.js
+++ functions/add_user_db.js
@@ -1,15 +1,16 @@
 const admin = require('firebase-admin');
 
-module.exports = function(req,res){
-    const db = admin.firestore();
-    const favorites = [];
-    db.collection('users').doc(req.body.email).set({
-        email: req.body.email,
-        fullName: req.body.displayName,
-        favorites: favorites
-    }).then(result => {
-        return res.send(result);
-    }).catch((error)=>{
-        res.status(422).send({error:error});
-    })
+module.exports = function (req, res) {
+	const db = admin.firestore();
+	db.collection('users')
+		.doc(req.body.email)
+        .set({
+            userId: req.body.email,
+			email: req.body.email,
+			fullName: req.body.displayName,
+		})
+		.then((result) => res.send(result))
+		.catch((error) => {
+			res.status(422).send({ error });
+		});
 };
--- functions/create_user.js
+++ functions/create_user.js
@@ -1,16 +1,16 @@
 const admin = require('firebase-admin');
 
-module.exports = function(req,res){
-    const displayName = req.body.firstName + " " + req.body.lastName;
+module.exports = function (req, res){
+    const displayName = req.body.firstName + ' ' + req.body.lastName;
 
     admin.auth().createUser({
         uid: req.body.email,
         email: req.body.email,
         password: req.body.password,
-        displayName: displayName
+        displayName,
       }).then((user)=>{
         return res.send(user);
-    }).catch((error)=>{
-        res.status(422).send({error:error});
+    }).catch((error) => {
+        res.status(422).send({ error });
     });
 };
