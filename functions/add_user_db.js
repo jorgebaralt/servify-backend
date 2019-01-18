2cc90cb5dc854d1e2773adc0cfac274490b9127d
jorgebaralt <jorgebaraltq@gmaik.com>
2019-01-18 18:21:35 -0500

    add user updated

--- functions/add_user_db.js
+++ functions/add_user_db.js
@@ -2,15 +2,35 @@ const admin = require('firebase-admin');
 
 module.exports = function (req, res) {
 	const db = admin.firestore();
+	const FieldValue = admin.firestore.FieldValue;
+
+	// check if user already exist
 	db.collection('users')
 		.doc(req.body.email)
-        .set({
-            userId: req.body.email,
-			email: req.body.email,
-			fullName: req.body.displayName,
+		.get()
+		.then((doc) => {
+			if (!doc.exist) {
+				const creationTime = FieldValue.serverTimestamp();
+				db.collection('users')
+					.doc(req.body.email)
+					.set({
+						uid: req.body.uid,
+						email: req.body.email,
+						fullName: req.body.displayName,
+						creationTime,
+						provider: req.body.provider,
+						photoURL: req.body.photoURL,
+						emailVerified: req.body.emailVerified
+					})
+					.then((result) => res.send(result))
+					.catch((error) => {
+						res.status(422).send({ error });
+					});
+			}
 		})
-		.then((result) => res.send(result))
 		.catch((error) => {
-			res.status(422).send({ error });
+			console.log(error);
+			res.send(422).send({ error });
 		});
+	// if not, craete new user
 };
