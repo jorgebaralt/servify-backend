e41c35da05357f5dea3372244b8e62ae28acbedf
Robert <rmolinamir@gmail.com>
2019-01-19 01:01:16 -0500

    Added CORS to get_rating.js

--- functions/get_ratings.js
+++ functions/get_ratings.js
@@ -1,26 +1,29 @@
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 module.exports = function (req, res) {
-	const db = admin.firestore();
-	const service = req.query;
-	let subcategory = '';
-	if (service.subcategory) {
-		subcategory = service.subcategory;
-	}
-	const documentName = service.email + '_' + service.category + '_' + subcategory;
-	db.collection('services')
-		.doc(documentName)
-		.collection('reviews')
-		.orderBy('timestamp')
-		.get()
-		.then((snapshot) => {
-			const query = [];
-			snapshot.forEach(doc => {
-				query.push(doc.data());
+    return cors(req, res, () => {
+		const db = admin.firestore();
+		const service = req.query;
+		let subcategory = '';
+		if (service.subcategory) {
+			subcategory = service.subcategory;
+		}
+		const documentName = service.email + '_' + service.category + '_' + subcategory;
+		db.collection('services')
+			.doc(documentName)
+			.collection('reviews')
+			.orderBy('timestamp')
+			.get()
+			.then((snapshot) => {
+				const query = [];
+				snapshot.forEach(doc => {
+					query.push(doc.data());
+				});
+				return res.send(query);
+			})
+			.catch((e) => {
+				res.status(422).send({ e });
 			});
-			return res.send(query);
-		})
-		.catch((e) => {
-			res.status(422).send({ e });
-		});
+    });
 };
