7f228d07eb4c05035cebb23d5477f200eb60ce62
jorgebaralt <jorgebaraltq@gmaik.com>
2019-01-25 23:13:01 -0500

    services now does near too

--- functions/services.js
+++ functions/services.js
@@ -9,10 +9,62 @@ module.exports = function (req, res) {
 			 * GET requests.
 			 */
 			case 'GET': {
-				const { category, subcategory, email, zipCode, id } = req.query;
+				const {
+					category,
+					subcategory,
+					email,
+					zipCode,
+					id,
+					distance
+				} = req.query;
+				let currentLocation;
 
+				if (req.query.currentLocation) {
+					currentLocation = JSON.parse(req.query.currentLocation);
+				}
+				
 				let field = '';
 				let value = '';
+
+				// Get near services
+				if (currentLocation && distance) {
+					const GeoPoint = admin.firestore.GeoPoint;
+					// Get user location (lat and long)
+
+					const lat = 0.0144927536231884;
+					const lon = 0.0181818181818182;
+
+					// Calculate geopoints according to distance that we want
+					const lowerLat = currentLocation.latitude - lat * distance;
+					const lowerLon = currentLocation.longitude - lon * distance;
+					const greaterLat =						currentLocation.latitude + lat * distance;
+					const greaterLon =						currentLocation.longitude + lon * distance;
+
+					// Pass to GeoPoints
+					const lesserGeopoint = new GeoPoint(lowerLat, lowerLon);
+					const greaterGeopoint = new GeoPoint(
+						greaterLat,
+						greaterLon
+					);
+
+					// Get only what it is in between those geopoints (firebase compares geopoints)
+					return db
+						.collection('services')
+						.where('location', '>=', lesserGeopoint)
+						.where('location', '<=', greaterGeopoint)
+						.get()
+						.then((snapshot) => {
+							const query = [];
+							snapshot.forEach((doc) => {
+								query.push(doc.data());
+							});
+							res.set(('Access-Control-Allow-Origin', '*'));
+							return res.send(query);
+						})
+						.catch((error) => {
+							return res.status(422).send({ error });
+						});
+				}
 
 				if (subcategory) {
 					field = 'subcategory';
@@ -32,7 +84,6 @@ module.exports = function (req, res) {
 				} else {
 					return res.status(422).send({ error: 'nothing to query' });
 				}
-
 				/*
 					get services for specific category/subcategory/email
 					take snapshot data, and put it into array
