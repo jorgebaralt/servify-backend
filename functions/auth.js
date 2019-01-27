e66de87772a03f652d71c1579614b6cfe294cf25
jorgebaralt <jorgebaraltq@gmaik.com>
2019-01-27 13:17:10 -0500

    working on auth

--- functions/auth.js
+++ functions/auth.js
@@ -1,17 +1,28 @@
 const admin = require('firebase-admin');
 
 module.exports = function (req, res) {
-	admin
-		.auth()
-		.createUser({
-			email: req.body.email,
-			password: req.body.password,
-			displayName: req.body.username,
-			photoURL: req.body.photoURL
-		})
-		.then((user) => res.send(user))
-        .catch((error) => {
-            console.log(error);
-			res.status(422).send({ error });
-		});
+	switch (req.method) {
+		/**
+		 * POST requests.
+		 */
+		case 'POST': {
+			return admin
+				.auth()
+				.createUser({
+					email: req.body.email,
+					password: req.body.password,
+					displayName: req.body.username,
+					photoURL: req.body.photoURL
+				})
+				.then((user) => res.send(user))
+				.catch((error) => {
+					console.log(error);
+					res.status(422).send({ error });
+				});
+		}
+		default:
+			return res.status(500).json({
+				message: 'Not allowed'
+			});
+	}
 };
