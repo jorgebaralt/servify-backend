17202b05326907035ac3b10054ac88665989fea0
jorgebaralt <jorgebaraltq@gmaik.com>
2019-02-01 13:26:18 -0500

    category option to add a new category | coding standard review

--- functions/categories.js
+++ functions/categories.js
@@ -4,20 +4,44 @@ const cors = require('cors')({ origin: true });
 module.exports = function (req, res) {
 	return cors(req, res, () => {
 		const db = admin.firestore();
-
-		db.collection('categories')
-			.orderBy('serviceCount', 'desc')
-			.limit(3)
-			.get()
-			.then((snapshot) => {
-				const query = [];
-				snapshot.forEach((doc) => {
-					query.push(doc.data());
+		switch (req.method) {
+			// creating a new categories to db
+			case 'POST': {
+				const { category } = req.body;
+				category.serviceCount = 0;
+				return db
+					.collection('categories')
+					.doc(category.dbReference)
+					.set(category)
+					.then(() => {
+						res.send('category added');
+					})
+					.catch((e) => {
+						console.log(e);
+						res.status(422).send(e);
+					});
+			}
+			case 'GET': {
+				return db
+					.collection('categories')
+					.orderBy('serviceCount', 'desc')
+					.limit(3)
+					.get()
+					.then((snapshot) => {
+						const query = [];
+						snapshot.forEach((doc) => {
+							query.push(doc.data());
+						});
+						return res.send(query);
+					})
+					.catch((e) => {
+						res.status(422).send(e);
+					});
+			}
+			default:
+				return res.status(500).json({
+					message: 'Not allowed'
 				});
-				return res.send(query);
-			})
-			.catch((e) => {
-				res.status(422).send(e);
-			});
+		}
 	});
 };
--- functions/review.js
+++ functions/review.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
 
-const calculateNewRatings = (oldRatings, review, bIsDelete, bIsPrice) => {
+const calculateNewRatings = (oldRatings, review, IsDelete, bIsPrice) => {
 	// Object keys
 	let average;
-	let	sum;
-	let	count;
+	let sum;
+	let count;
 	// Depending on the type of the rating the keys will change. The boolean bIsPrice
 	// determines which type of rating object will be returned.
 	if (bIsPrice) {
@@ -22,30 +22,28 @@ const calculateNewRatings = (oldRatings, review, bIsDelete, bIsPrice) => {
 		[sum]: oldRatings[sum] ? oldRatings[sum] : 0,
 		[count]: oldRatings[count] ? oldRatings[count] : 0
 	};
-    // If it's delete then the count decreses by 1. Else it will add 1.
-    if (bIsDelete) {
-        ratings[count]--;
-    } else {
-        ratings[count]++;
-    }
-    // If it's a post, then sum. Else it will be a difference between the old sum
-    // and the deleted review's rating.
-    if (bIsDelete) {
-        ratings[sum] -= review[average];
-    } else {
-        ratings[sum] += review[average];
-    }
-    ratings[average] = ( // Average
-        (ratings[sum]) / (ratings[count])
-    );
-    return ratings;
+	// If it's delete then the count decreses by 1. Else it will add 1.
+	if (IsDelete) {
+		ratings[count]--;
+	} else {
+		ratings[count]++;
+	}
+	// If it's a post, then sum. Else it will be a difference between the old sum
+	// and the deleted review's rating.
+	if (IsDelete) {
+		ratings[sum] -= review[average];
+	} else {
+		ratings[sum] += review[average];
+	}
+	ratings[average] = ratings[sum] / ratings[count]; // Average
+	return ratings;
 };
 
 const washRatings = (updatedRatings, bIsPrice) => {
 	// Object keys
 	let average;
-	let	sum;
-	let	count;
+	let sum;
+	let count;
 	// Depending on the type of the rating the keys will change. The boolean bIsPrice
 	// determines which type of rating object will be returned.
 	if (bIsPrice) {
@@ -84,65 +82,85 @@ module.exports = function (req, res) {
 				const { review, serviceId } = req.body;
 				review.timestamp = FieldValue.serverTimestamp();
 				// Checking if review exists under this service ID
-				return db.collection('reviews')
+				return db
+					.collection('reviews')
 					.where('uid', '==', review.uid)
 					.where('serviceId', '==', serviceId)
 					.get()
-						.then((snapshot) => {
-							// If it's empty, create the review.
-							// Firestore generates new id if executing doc() with no arguments.
-                            if (snapshot.empty) {
-								const ref = db.collection('reviews').doc();
-								review.id = ref.id;
-								return ref.set(review).then(() => {
-									db.collection('services')
-										.doc(serviceId)
-										.get()
-										.then((doc) => {
-											let newRatings = calculateNewRatings({
-												ratingCount: doc.data().ratingCount,
+					.then((snapshot) => {
+						// If it's empty, create the review.
+						// Firestore generates new id if executing doc() with no arguments.
+						if (snapshot.empty) {
+							const ref = db.collection('reviews').doc();
+							review.id = ref.id;
+							return ref.set(review).then(() => {
+								db.collection('services')
+									.doc(serviceId)
+									.get()
+									.then((doc) => {
+										let newRatings = calculateNewRatings(
+											{
+												ratingCount: doc.data()
+													.ratingCount,
 												ratingSum: doc.data().ratingSum,
 												rating: doc.data().rating
-											}, review);
-											let newPriceRatings = calculateNewRatings({
-												priceCount: doc.data().priceCount,
+											},
+											review
+										);
+										let newPriceRatings = calculateNewRatings(
+											{
+												priceCount: doc.data()
+													.priceCount,
 												priceSum: doc.data().priceSum,
 												price: doc.data().price
-											}, review, false, true);
-											// Avoid errors and going negative.
-											newRatings = washRatings(newRatings);
-											newPriceRatings = washRatings(newPriceRatings, true);
-											db.collection('services')
-												.doc(serviceId)
-												.update({
-													ratingCount: newRatings.ratingCount,
-													ratingSum: newRatings.ratingSum,
-													rating: newRatings.rating,
-													priceCount: newPriceRatings.priceCount,
-													priceSum: newPriceRatings.priceSum,
-													price: newPriceRatings.price
-												})
-												.then(() => res.status(200).send(review))
-												.catch((error) => {
-													console.log(error); // Firebase log
-													return res.status(422).send(error);
-												});
-										})
-										.catch((error) => {
-											console.log(error); // Firebase log
-											return res.status(422).send(error);
-										});
-								});
-							}
-							return res.status(422).send({
-								error: 'This account already has made a review to this service. Only 1 review per service is allowed.',
-								type: 'warning'
+											},
+											review,
+											false,
+											true
+										);
+										// Avoid errors and going negative.
+										newRatings = washRatings(newRatings);
+										newPriceRatings = washRatings(
+											newPriceRatings,
+											true
+										);
+										db.collection('services')
+											.doc(serviceId)
+											.update({
+												ratingCount:
+													newRatings.ratingCount,
+												ratingSum: newRatings.ratingSum,
+												rating: newRatings.rating,
+												priceCount:
+													newPriceRatings.priceCount,
+												priceSum:
+													newPriceRatings.priceSum,
+												price: newPriceRatings.price
+											})
+											.then(() => res.status(200).send(review))
+											.catch((error) => {
+												console.log(error); // Firebase log
+												return res
+													.status(422)
+													.send(error);
+											});
+									})
+									.catch((error) => {
+										console.log(error); // Firebase log
+										return res.status(422).send(error);
+									});
 							});
-						})
-						.catch((error) => {
-							console.log(error); // Firebase log
-							return res.status(422).send(error);
+						}
+						return res.status(422).send({
+							error:
+								'This account already has made a review to this service. Only 1 review per service is allowed.',
+							type: 'warning'
 						});
+					})
+					.catch((error) => {
+						console.log(error); // Firebase log
+						return res.status(422).send(error);
+					});
 			}
 			/**
 			 * DELETE requests.
@@ -158,20 +176,33 @@ module.exports = function (req, res) {
 							.doc(review.serviceId)
 							.get()
 							.then((doc) => {
-								let newRatings = calculateNewRatings({
-									ratingCount: doc.data().ratingCount,
-									ratingSum: doc.data().ratingSum,
-									rating: doc.data().rating
-								}, review, true);
-								let newPriceRatings = calculateNewRatings({
-									priceCount: doc.data().priceCount,
-									priceSum: doc.data().priceSum,
-									price: doc.data().price
-								}, review, true, true);
+								let newRatings = calculateNewRatings(
+									{
+										ratingCount: doc.data().ratingCount,
+										ratingSum: doc.data().ratingSum,
+										rating: doc.data().rating
+									},
+									review,
+									true
+								);
+								let newPriceRatings = calculateNewRatings(
+									{
+										priceCount: doc.data().priceCount,
+										priceSum: doc.data().priceSum,
+										price: doc.data().price
+									},
+									review,
+									true,
+									true
+								);
 								// Avoid errors and going negative.
 								newRatings = washRatings(newRatings);
-								newPriceRatings = washRatings(newPriceRatings, true);
-								return db.collection('services')
+								newPriceRatings = washRatings(
+									newPriceRatings,
+									true
+								);
+								return db
+									.collection('services')
 									.doc(review.serviceId)
 									.update({
 										ratingCount: newRatings.ratingCount,
